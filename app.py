2525957cabc3c924cd70a1c0627a902f6fe6bb9e
PedroDuque04 <phdferrarini@gmail.com>
2026-06-11 10:11:37 -0300

    criando histograma e um gráfico

--- app.py
+++ app.py
@@ -3,12 +3,20 @@ import streamlit as st
 import plotly.express as px
 
 car_data = pd.read_csv('vehicles.csv')
-hist_button = st.checkbox('Criar Histograma')
+hist_checkbox = st.checkbox('Criar Histograma')
 
-if hist_button:
+if hist_checkbox:
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carro')
 
     fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+hist_button = st.checkbox('Criar Gráfico')
+
+if hist_button:
+    fig = px.scatter(car_data, x="odometer", y="price")
 
     st.plotly_chart(fig, use_container_width=True)

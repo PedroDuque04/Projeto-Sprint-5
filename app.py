3d43c3943b177eb9d2b0d07c229b819a54cc036d
PedroDuque04 <phdferrarini@gmail.com>
2026-06-11 00:17:57 -0300

    Criando um histograma

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('C:/Users/duque/Downloads/vehicles.csv')
+hist_button = st.checkbox('Criar Histograma')
+
+if hist_button:
+    st.write(
+        'Criando um histograma para o conjunto de dados de anúncios de vendas de carro')
+
+    fig = px.histogram(car_data, x="odometer")
+
+    st.plotly_chart(fig, use_container_width=True)
